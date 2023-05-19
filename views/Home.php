a392576ca3d6ec9246824fe683f693654c56fc53
Dominik Husák <domca.husak@gmail.com>
2023-05-19 10:21:44 +0200

    HomePage

--- views/Home.php
+++ views/Home.php
@@ -1,12 +1,9 @@
-<main>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
-<p>/</p>
+<main id="main1">
+    <div class="d-flex flex-wrap justify-content-center text-white">
+        <p id="p1">Welcome to CryptoCurrency</p>
+        <p id="p2">Enter the world of digital money and explore the possibilities offered by cryptocurrencies. 
+        Here you will find up-to-date information, analyses, and guides that will help you understand 
+        the world of cryptocurrencies and harness their potential.
+        </p>
+    </div>
 </main>
--- views/core/header.php
+++ views/core/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/waprojekt/views/img/favicon.ico">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 </head>
@@ -26,6 +27,9 @@
             </li>
             <li class="nav-item">
             <a class="nav-link text-white bg-dark" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white bg-dark" href="#">Currency</a>
             </li>
         </ul>
         </div>
