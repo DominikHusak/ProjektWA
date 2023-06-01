2027646d3cfb7cf0181665d800e2b652eef6d130
Dominik Husák <domca.husak@gmail.com>
2023-06-01 10:02:30 +0200

    Prices-QuestionsAccordion-Upgrade

--- views/PriceOverview.php
+++ views/PriceOverview.php
@@ -2,7 +2,7 @@
   <div class="class d-flex text-white justify-content-center">
     <p class="mt-4" id="p3">Current prices</p>
   </div>
-  <div class="d-flex justify-content-center flex-column align-items-center mt-5" id="tableDiv">
+  <div class="d-flex justify-content-center flex-column align-items-center mt-2 mx-3" id="tableDiv">
     <table class="table table-striped table-dark" id="cryptoCurrencyTable">
         <thead>
           <tr>
@@ -16,90 +16,97 @@
         </tbody>
     </table>
   </div>
-  <div class="d-flex justify-content-evenly align-items-center">
-  <div class="accordion my-5" id="accordionExample">
+  <div class="d-flex justify-content-center flex-column align-items-center mt-5">
+  <h1 class="text-white">Most asked questions</h1>
+  <div class="d-flex">
+  <div class="accordion mt-3 mb-5" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        Accordion Item #1
+      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        How safe is cryptocurrency?
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin 
-        adds the ap
+      Despite its decentralized nature, transactions on most cryptocurrency networks are 
+      very secure — as long as crypto users take precautions. 
+      The underlying blockchain technology is inherently secure.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+        Who created Bitcoin?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+        Bitcoin was first described in a white paper released in 2008 and published under 
+        the name “Satoshi Nakamoto.” The protocol underlying Bitcoin was released in 2009 as 
+        open source software2. The Bitcoin network is a worldwide network of computers that run Bitcoin software.      </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+        What is bitcoin mining?
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      Bitcoin mining is the mechanism by which participants on the Bitcoin network reach 
+      consensus regarding the next block of transactions to add to the Bitcoin blockchain. 
+      For each new block, certain people called “miners” race with one another to find a 
+      solution to a cryptographic puzzle. The first miner to find a solution gets to propose 
+      the new block of transactions to the entire network. As a reward, the winning miner of 
+      the block gets newly created bitcoin. Thus, mining is also the process by which new 
+      bitcoin are introduced into the system.      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        Who controls Bitcoin’s software?
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Bitcoin is an open source project, with no official entity controlling the Bitcoin network. 
+      Anyone can review Bitcoin’s code and propose changes. There are many compatible versions 
+      of Bitcoin network software, but a distribution of the software known as “Bitcoin Core” is 
+      currently the most widely adopted and provides the de facto standard for the Bitcoin protocol.      
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+        Is cryptocurrency traceable?
+      </button>
+    </h2>
+    <div id="collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Transactions on public blockchains like Bitcoin, although pseudonymous, are transparent 
+      and publicly viewable. Anyone can see all transactions linked to any address on the Bitcoin blockchain.
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header">
-      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #4
+      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+        Are cryptocurrencies used for illegal purposes?
       </button>
     </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+    <div id="collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      Cryptocurrencies operate on a decentralized network, which means it lacks a centralized authority. 
+      Crypto holders can perform transactions without actually revealing their identities, which is 
+      why the market gained a reputation to be a hub for illegal practitioners. But, the blockchain 
+      network publicly records every transaction. These transactions or payments can be traced back 
+      to a crypto exchange that can identify the end-users.
       </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #5
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button bg-dark text-white collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #6
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-<div>
-  <div class="card" style="width: 18rem;">
-  <div class="card-body bg-dark text-white">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 </div>
