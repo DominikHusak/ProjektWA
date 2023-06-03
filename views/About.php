cbdd86778678490feaa7c03d68bf9a50d8e17e94
Dominik Husák <domca.husak@gmail.com>
2023-06-03 20:10:12 +0200

    AboutUs-CardsChange

--- views/About.php
+++ views/About.php
@@ -1,110 +1,144 @@
 <main id="main2">
 <div class="p-5">
-<div class="container">
-  <div class="row justify-content-end">
-    <div class="col-md-8">
-      <div class="card mb-5 pr-5">
-        <img src="/views/img/107069560-1654078537245-gettyimages-1235570446-porzycki-cryptocu210928_npIa0.jpeg" class="card-img-top">
-        <div class="card-body bg-dark text-white">
-          <h5 class="card-title"><ins>What is Crypto</ins></h5>
-          <p class="card-text">Cryptocurrency, or crypto for short, is a digital or virtual form of money that is secured using cryptography. It is a decentralized system that enables secure and anonymous transactions directly between individual network participants, without the need for an intermediary such as a bank or payment gateway. Cryptocurrencies utilize blockchain technology, which is a distributed public ledger system that records all transactions and ensures their immutability. Cryptocurrencies offer the potential for fast and inexpensive transactions, investment opportunities, and also drive innovation in the fields of finance and technology.</p>
+  <div class="container">
+    <div class="card bg-dark text-white mb-5">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="/views/img/107069560-1654078537245-gettyimages-1235570446-porzycki-cryptocu210928_npIa0.jpeg" class="img-fluid rounded-start" style="height: 100%; width: 100%;">
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="row justify-content-start">
-    <div class="col-md-8">
-      <div class="card mb-5 pr-5">
-        <img src="/views/img/DR72FGFNKVA6NF6JTWSSEHTJ3M.jpg" class="card-img-top">
-        <div class="card-body bg-dark text-white">
-          <h5 class="card-title"><ins>What makes the price</ins></h5>
-          <p class="card-text">The value of cryptocurrencies is determined by several factors. One of the main factors is supply and demand in the market. When there is increasing interest in a particular cryptocurrency and more people are willing to buy it, the price tends to rise. Conversely, if the demand decreases and people sell their cryptocurrencies, the price falls. Another factor influencing value is investor trust. If a cryptocurrency gains support from institutional investors or is adopted on a larger scale, it can strengthen its value.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row justify-content-end">
-    <div class="col-md-8">
-      <div class="card mb-5">
-        <div class="d-flex justify-content-center my-3">
-          <div id="carouselExample" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="/views/img/bitcoin-logo.png" class="d-block w-100" alt="Bitcoin">
-              </div>
-              <div class="carousel-item">
-                <img src="/views/img/ethereum-logo.png" class="d-block w-100" alt="Ethereum">
-              </div>
-              <div class="carousel-item">
-                <img src="/views/img/cardano-logo.png" class="d-block w-100" alt="Cardano">
-              </div>
-              <div class="carousel-item">
-                <img src="/views/img/solana-logo.png" class="d-block w-100" alt="Solana">
-              </div>
-              <div class="carousel-item">
-                <img src="/views/img/litecoin-logo.png" class="d-block w-100" alt="Litecoin">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h1 class="card-title"><ins>What is crypto</ins></h1>
+            <p class="card-text">Cryptocurrency, or crypto for short, is a digital or virtual form of 
+                  money that is secured using cryptography. It is a decentralized system that enables 
+                  secure and anonymous transactions directly between individual network participants, without 
+                  the need for an intermediary such as a bank or payment gateway. Cryptocurrencies utilize 
+                  blockchain technology, which is a distributed public ledger system that records all 
+                  transactions and ensures their immutability.</p>
           </div>
         </div>
-        <div class="card-body bg-dark text-white">
-          <h5 class="card-title"><ins>Types of Crypto</ins></h5>
-          <p class="card-text">There is a wide range of cryptocurrencies in existence. Bitcoin, the first and most well-known cryptocurrency, operates on a decentralized payment system. Ethereum, another prominent cryptocurrency, focuses on supporting smart contracts and decentralized applications. Other popular cryptocurrencies include Ripple, Litecoin, Bitcoin Cash, and Cardano. Each of these cryptocurrencies has its own specific features, technologies, and focus, offering various opportunities for investment and utilization in different areas.</p>
+      </div>
+    </div>
+
+    <div class="card bg-dark text-white mb-5">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="/views/img/DR72FGFNKVA6NF6JTWSSEHTJ3M.jpg" class="img-fluid rounded-start" style="height: 100%; width: 100%;">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h2 class="card-title"><ins>What makes the price</ins></h2>
+            <p class="card-text">The value of cryptocurrencies is determined by several factors. One of the main 
+              factors is supply and demand in the market. When there is increasing interest in a particular 
+              cryptocurrency and more people are willing to buy it, the price tends to rise. Conversely, if 
+              the demand decreases and people sell their cryptocurrencies, the price falls. Another factor 
+              influencing value is investor trust. If a cryptocurrency gains support from institutional 
+              investors or is adopted on a larger scale, it can strengthen its value.</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row justify-content-start">
-    <div class="col-md-8">
-      <div class="card mb-5 pr-5">
-        <img src="/views/img/Cryptocurrency_SHARE-03-1.jpg" class="card-img-top">
-        <div class="card-body bg-dark text-white">
-          <h5 class="card-title"><ins>Our goal</ins></h5>
-          <p class="card-text">The goal of our website is to provide users with accurate and up-to-date information about cryptocurrency prices. We monitor the cryptocurrency market 24 hours a day and offer instant real-time price updates. Our website is designed to be user-friendly and intuitive, allowing you to quickly and easily find the information you need. We want to ensure that you are always well-informed about the latest developments in the cryptocurrency market so that you can make informed decisions and invest effectively.</p>
+
+    <div class="card bg-dark text-white mb-5">
+      <div class="row g-0">
+        <div class="col-md-4">
+            <div id="carouselExample" class="carousel slide bg-white rounded">
+              <div class="carousel-inner d-flex align-items-center justify-content-center">
+                <div>
+                  <div class="carousel-item active">
+                    <img src="/views/img/bitcoin-logo.png" class="d-block" alt="Bitcoin">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/views/img/ethereum-logo.png" class="d-block" alt="Ethereum">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/views/img/cardano-logo.png" class="d-block" alt="Cardano">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/views/img/solana-logo.png" class="d-block" alt="Solana">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="/views/img/litecoin-logo.png" class="d-block" alt="Litecoin">
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev bg-dark" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              </button>
+              <button class="carousel-control-next bg-dark" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              </button>
+            </div>
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h3 class="card-title"><ins>Types of Crypto</ins></h3>
+            <p class="card-text">There is a wide range of cryptocurrencies in existence. Bitcoin, the first 
+              and most well-known cryptocurrency, operates on a decentralized payment system. Ethereum, another 
+              prominent cryptocurrency, focuses on supporting smart contracts and decentralized 
+              applications. Other popular cryptocurrencies include Ripple, Litecoin, Bitcoin Cash, and 
+              Cardano. Each of these cryptocurrencies has its own specific features, technologies, and 
+              focus, offering various opportunities for investment and utilization in different areas. 
+              Cryptocurrencies, also known as digital or virtual currencies, have emerged as a 
+              transformative force in the financial landscape.</p>
+          </div>
         </div>
       </div>
     </div>
+
+    <div class="card bg-dark text-white mb-5">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="/views/img/cryptoGoals.jpg" class="img-fluid rounded-start" style="height: 100%; width: 100%;">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h4 class="card-title"><ins>Our goal</ins></h4>
+            <p class="card-text">The goal of our website is to provide users with accurate and up-to-date 
+              information about cryptocurrency prices. We monitor the cryptocurrency market 24 hours a 
+              day and offer instant real-time price updates. Our website is designed to be user-friendly 
+              and intuitive, allowing you to quickly and easily find the information you need. We want to 
+              ensure that you are always well-informed about the latest developments in the cryptocurrency 
+              market so that you can make informed decisions and invest effectively. Cryptocurrency prices 
+              are highly volatile and can dramatically change within a short period of time. Our platform 
+              also offers general information about cryptocurrencies.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
-</div>
 
   <div id="ourPros" class="text-white d-flex align-items-center flex-column">
   <h2>Why Choose Us</h2>
         <div class="row mt-3">
           <div class="col-md-6">
-            <div class="card mb-4">
-              <div class="card-body bg-dark text-white">
+            <div class="card mb-4 bg-dark text-white">
+              <div class="card-body">
                 <h5 class="card-title"><ins>Comprehensive Guides</ins></h5>
                 <p class="card-text">Our platform offers comprehensive guides on various cryptocurrencies, covering their history, technology, and potential applications.</p>
               </div>
             </div>
           </div>
           <div class="col-md-6">
-            <div class="card mb-4">
-              <div class="card-body bg-dark text-white">
+            <div class="card mb-4 bg-dark text-white">
+              <div class="card-body">
                 <h5 class="card-title"><ins>Market Insights</ins></h5>
                 <p class="card-text">Stay up-to-date with the latest market trends and insights provided by our team of experts, helping you make informed decisions.</p>
               </div>
             </div>
           </div>
           <div class="col-md-6">
-            <div class="card mb-4">
-              <div class="card-body bg-dark text-white">
+            <div class="card mb-4 bg-dark text-white">
+              <div class="card-body">
                 <h5 class="card-title"><ins>Community Support<ins></h5>
                 <p class="card-text">Join our vibrant community of cryptocurrency enthusiasts, where you can connect, share ideas, and ask questions.</p>
               </div>
             </div>
           </div>
           <div class="col-md-6">
-            <div class="card">
-              <div class="card-body bg-dark text-white">
+            <div class="card bg-dark text-white">
+              <div class="card-body">
                 <h5 class="card-title"><ins>Secure Transactions<ins></h5>
                 <p class="card-text">Learn how to securely buy, sell, and trade cryptocurrencies, ensuring the safety of your transactions.</p>
               </div>
